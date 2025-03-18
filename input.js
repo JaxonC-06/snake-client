69eb686dba677cdf13beae5bd09ceb431ed3b976
JaxonC-06 <jaxoncostain@outlook.com>
2025-03-18 14:36:03 -0600

    Added working messages, binded to keys 1-5

--- input.js
+++ input.js
@@ -14,7 +14,12 @@ const keyMap = {
   w: 'Move: up',
   a: 'Move: left',
   s: 'Move: down',
-  d: 'Move: right'
+  d: 'Move: right',
+  '1': "Say: Hey everyone!",
+  '2': "Say: See you later!",
+  '3': "Say: Oh, so close!",
+  '4': "Say: LOL",
+  '5': "Say: gotcha!"
 };
 
 const handleUserInput = function (key) {
